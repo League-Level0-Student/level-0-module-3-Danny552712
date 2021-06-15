25cf138e2a9cd3f166f1a8c0d9027d10fb55c3f5
student <student@ip-172-31-59-194.us-west-2.compute.internal>
2021-06-15 15:23:00 -0700

    d

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -3,19 +3,67 @@ package _01_else_if._1_robot_color_chooser;
 
 import javax.swing.JOptionPane;
 import java.awt.Color;
+import org.jointheleague.graphical.robot.Robot;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		
 		//1. Create a new Robot
+		Robot bob = new Robot();
 	    //2. Make the robot draw a shape (this will take more than one line of code)		
-		//3. Set the pen width to 10
-		//4. Ask the user what color pen they would like the robot to draw with
-		//5. Use an if/else statement to set the pen color that the user requested
-        //6. If the user doesn't enter anything, choose a random color
-        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
+		for (int loop = 0; loop < 10; loop++) {
+		String penColor = JOptionPane.showInputDialog("What is you favorite color?");
+        
 		
-
+		//3. Set the pen width to 10
+		
+		//4. Ask the user what color pen they would like the robot to draw with
+		
+		//5. Use an if/else statement to set the pen color that the user requested
+        if(penColor .equals ("green")) {
+        	bob.setPenColor(0,250,0);
+        }
+        else if (penColor.equals("blue")) {
+        	bob.setPenColor(0,0,250);
+        }
+        else if (penColor.equals("red")) {
+        	bob.setPenColor(250,0,0);
+        }
+        else if (penColor.equals("yellow")) {
+        	bob.setPenColor(250,250,0);
+        }
+        else if (penColor.equals("orange")) {
+        	bob.setPenColor(250,150,0);
+        }
+        else if (penColor.equals("purple")) {
+        	bob.setPenColor(200,0,250);
+        }
+        else if (penColor.equals("pink")) {
+        	bob.setPenColor(250,0,200);
+        }
+        else if (penColor.equals("white")) {
+        	bob.setPenColor(250,250,250);
+        }
+        else if (penColor.equals("black")) {
+        	bob.setPenColor(0,0,0);
+        }
+		//6. If the user doesn't enter anything, choose a random color
+        else {
+        	bob.setRandomPenColor();
+        }
+        
+        bob.setPenWidth(10);
+        bob.penDown();
+		bob.setSpeed(50);
+		bob.hide();
+		bob.move(160);
+		bob.turn(120);
+		bob.move(160);
+		bob.turn(120);
+		bob.move(160);
+		//7. Put a loop around your code so that you keep asking the user for more colors & drawing them
+		}
+        
 		
 
 
--- src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
+++ src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
@@ -3,17 +3,28 @@ package _01_else_if._2_crazy_cat_lady;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user how many cats they have
+		String catCount = JOptionPane.showInputDialog("How many cats do you have");
 		// 2. Convert their answer into an int
+	int catCountInt = Integer.parseInt(catCount);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
+	if  (catCountInt >= 3) {
+		JOptionPane.showMessageDialog(null, "You are a crazy cat person.");
+	}
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
 		//    Hint: You can use the && operator 
+	else if (catCountInt < 3 && catCountInt > 0) {
+		JOptionPane.showMessageDialog(null, "Cat");
+	}
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
-		
-		
+	else if (catCountInt == 0) {
+	JOptionPane.showMessageDialog(null, "No cats for you.");
+	}
 		
 		
 		
